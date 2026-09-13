6d3f01e890c3e4bda304e6fad0ea8365debf3875
Shashank907 <shashankshekharp97@gmail.com>
2026-09-14 00:00:57 +0530

    Dockerize hospital management system

--- hospitalSystem/src/main/java/com/shashankcdr/hospitalSystem/security/AuthService.java
+++ hospitalSystem/src/main/java/com/shashankcdr/hospitalSystem/security/AuthService.java
@@ -7,7 +7,7 @@ import com.shashankcdr.hospitalSystem.dto.SignupResponseDto;
 import com.shashankcdr.hospitalSystem.entity.User;
 import com.shashankcdr.hospitalSystem.entity.type.Role;
 import com.shashankcdr.hospitalSystem.repository.UserRepository;
-import com.sun.jdi.request.DuplicateRequestException;
+import com.shashankcdr.hospitalSystem.exception.ResourceAlreadyExistsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -39,7 +39,7 @@ public class AuthService {
     public SignupResponseDto signup(SignupRequestDto signupRequestDto) {
         User user=userRepository.findByUsername(signupRequestDto.getUsername()).orElse(null);
 
-        if(user !=null) throw new DuplicateRequestException("User already exists");
+        if(user !=null) throw new ResourceAlreadyExistsException("User already exists");
 
         user=userRepository.save(User.builder()
                 .username(signupRequestDto.getUsername())
