5f2be224270c5378e51b52dfd4d590ff98796de1
Shashank907 <shashankshekharp97@gmail.com>
2026-09-15 16:26:59 +0530

    refactor: restructure department module

---
+++ hospitalSystem/src/main/java/com/shashankcdr/hospitalSystem/department/entity/Department.java
@@ -0,0 +1,35 @@
+package com.shashankcdr.hospitalSystem.department.entity;
+
+import com.shashankcdr.hospitalSystem.doctor.entity.Doctor;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+
+    @OneToOne
+    private Doctor headDoctor;
+
+    @ManyToMany
+    @JoinTable(
+            name = "my_dpt_doctors",
+            joinColumns = @JoinColumn(name = "dpt_id"),
+            inverseJoinColumns = @JoinColumn(name = "doctor_id")
+    )
+    private Set<Doctor> doctors = new HashSet<>();
+}
